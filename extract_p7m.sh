0f469454c06ea7455b4d3ea7b85fdfe73a51ada0
Luca Postregna <luca.postregna@gmail.com>
2016-10-22 14:59:45 +0200

    fixed p7m output with white space in name

--- extract_p7m.sh
+++ extract_p7m.sh
@@ -1,2 +1,10 @@
-#!/bin/sh
-openssl smime -decrypt -in $1 -inform DER -verify -noverify -out $2
+#!/bin/bash
+
+rename "s/\s+/_/g" *
+
+for file in *.P7M
+do 
+	output=`basename $file .P7M`
+	openssl smime -decrypt -in $file -inform DER -verify -noverify -out $output
+done
+
