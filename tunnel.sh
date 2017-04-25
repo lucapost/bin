86538b98447b23341e9b950ef13fcfcb6dbca680
Luca Postregna <luca.postregna@gmail.com>
2017-04-25 11:44:47 +0200

    fix

--- tunnel.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-autossh -f -N -M 40001 -L 33001:127.0.0.1:22000   -f lucapost@vultr        -p 4444
-autossh -f -N -M 40002 -L 33002:127.0.0.1:22000   -f lucapost@vultr2        -p 22
-#autossh -f -N -M 40001 -L 33002:192.168.0.2:22000 -f root@koslic.ninux.org -p 4444
