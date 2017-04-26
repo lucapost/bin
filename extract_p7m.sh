5e6b10e9648ea5a16f6fb853c13c9154b181fbf4
Luca Postregna <luca.postregna@gmail.com>
2017-04-26 08:51:20 +0200

    fix

--- extract_p7m.sh
+++ extract_p7m.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+cd ~/Downloads
+
 rename "s/\s+/_/g" *
 
 for file in *.P7M
+do 
+	output=`basename $file .P7M`
+	openssl smime -decrypt -in $file -inform DER -verify -noverify -out $output
+done
+
+for file in *.p7m
 do 
 	output=`basename $file .P7M`
 	openssl smime -decrypt -in $file -inform DER -verify -noverify -out $output
--- tunnel.sh
+++ tunnel.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-autossh -f -N -M 40001 -L 33001:127.0.0.1:22000   -f lucapost@vultr        -p 4444
-autossh -f -N -M 40002 -L 33002:127.0.0.1:22000   -f lucapost@vultr2        -p 22
-#autossh -f -N -M 40001 -L 33002:192.168.0.2:22000 -f root@koslic.ninux.org -p 4444
+autossh -f -N -M 40004 -R 4447:localhost:22     -f lucapost@linode
+autossh -f -N -M 40002 -L 33002:127.0.0.1:22000 -f lucapost@linode        -p 22
+autossh -f -N -M 40003 -L 33003:127.0.0.1:22000 -f lucapost@qnap.dreka.it -p 22
