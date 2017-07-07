acd36c410e43bfd02c223530f9ffe0e24c28e8b0
Luca Postregna <luca.postregna@gmail.com>
2017-07-07 15:19:48 +0200

    update_compress_pdf

--- compress-pdf.sh
+++ compress-pdf.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dDownsampleColorImages=true -dColorImageResolution=200 -dNOPAUSE  -dBATCH -sOutputFile=$2 $1
+#gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dDownsampleColorImages=true -dColorImageResolution=200 -dNOPAUSE  -dBATCH -sOutputFile=$2 $1
+
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$2 $1
